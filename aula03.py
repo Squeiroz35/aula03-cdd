6630696cdda02ee87460d4f0084641c21136ab92
Sérgio Queiroz <sergio08queiroz@gmail.com>
2025-03-31 14:14:34 -0300

    novo projeto

---
+++ aula03.py
@@ -0,0 +1,4 @@
+name= input("type your name: ")
+age= int(input("type your age: "))
+wage= float(input("type your wage current: "))
+print (f"welcome {name}, your age is: {age}, and your wage is {wage}:")
