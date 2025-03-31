772abcecf5b875f27b6e600306ca4a8a5472bab9
Sérgio Queiroz <sergio08queiroz@gmail.com>
2025-03-31 15:11:44 -0300

    nova atualização

--- exercicio.2.py
+++ exercicio.2.py
@@ -1,10 +1,10 @@
 n1= float(input("digite o primeiro numero"))
 n2= float(input("digite o segundo numero"))
 adicao= n1+n2
-subitracao=n1-n2
+subitracao= n1-n2
 mutiplicacao= n1*n2
 divisao= n1/n2
-print (adicao)
-print (subitracao)
-print (mutiplicacao)
-print(divisao)
+print (f"O resultado da soma foi: {adicao}")
+print (f"O resultado da subitração foi: {subitracao})
+print (f"O resultado da mutiplicação foi: {mutiplicacao})
+print(f"O resultado da divisão foi: {divisao})
