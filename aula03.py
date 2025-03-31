a47307bdae087278b9ee23c0c8c0b683367259c3
Sérgio Queiroz <sergio08queiroz@gmail.com>
2025-03-31 14:46:08 -0300

    atualizando codigo

--- aula03.py
+++ aula03.py
@@ -1,4 +1,9 @@
-name= input("type your name: ")
-age= int(input("type your age: "))
-wage= float(input("type your wage current: "))
+name= input("digite seu nome: ")
+age= int(input("dgite sua idade: "))
+wage= float(input("digite seu salario: "))
 print (f"welcome {name}, your age is: {age}, and your wage is {wage}:")
+aumento= float(input("digite em quantos porcentos foi o aumento do seu salario:"))
+valoraumento = wage*(aumento/100)
+print(f"seu aumento foi de: {valoraumento}")
+salarionovo= wage+valoraumento
+print(f"seu salario depois do aumento é: {salarionovo}")
