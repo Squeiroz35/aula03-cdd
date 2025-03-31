571497f17bd0dc23607006c8019696d22a52df08
Sérgio Queiroz <sergio08queiroz@gmail.com>
2025-03-31 15:59:09 -0300

    exercicio3

--- exercicio.2.py
+++ exercicio.2.py
@@ -5,6 +5,6 @@ subitracao= n1-n2
 mutiplicacao= n1*n2
 divisao= n1/n2
 print (f"O resultado da soma foi: {adicao}")
-print (f"O resultado da subitração foi: {subitracao})
-print (f"O resultado da mutiplicação foi: {mutiplicacao})
-print(f"O resultado da divisão foi: {divisao})
+print (f"O resultado da subitração foi: {subitracao}")
+print (f"O resultado da mutiplicação foi: {mutiplicacao}")
+print(f"O resultado da divisão foi: {divisao}")
---
+++ exercicio.3.py
@@ -0,0 +1,8 @@
+A=5
+B=10
+X=0
+print (f"valor de A é: {A}, valor de B é {B}")
+X = A
+A = B
+B = X
+print (f"valor de A é:{A},valor de B é: {B}")
