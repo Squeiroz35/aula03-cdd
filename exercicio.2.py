b742e13f9ffdbca50775247589cefbc6591f2828
Sérgio Queiroz <sergio08queiroz@gmail.com>
2025-03-31 14:55:12 -0300

    novo arquivo

---
+++ exercicio.2.py
@@ -0,0 +1,10 @@
+n1= float(input("digite o primeiro numero"))
+n2= float(input("digite o segundo numero"))
+adicao= n1+n2
+subitracao=n1-n2
+mutiplicacao= n1*n2
+divisao= n1/n2
+print (adicao)
+print (subitracao)
+print (mutiplicacao)
+print(divisao)
