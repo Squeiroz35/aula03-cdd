a1931349ba6f81ef88165b9719ef70a3efda4143
Sérgio Queiroz <sergio08queiroz@gmail.com>
2025-04-02 14:21:53 -0300

    atualização do exercicio7

--- exercicio7.py
+++ exercicio7.py
@@ -4,10 +4,10 @@ tipo= input("qual o tipo de combustivel voce deseja?")
 qntdecombustivel= float(input("digite quantos litros voce desejar colocar:"))
 if tipo== "e":
     valor= E * qntdecombustivel
-    print (f"total {valor}")
+    print (f"total de litros: {valor}")
 elif tipo== "g":
     valor= G * qntdecombustivel
-    print (f"total {valor} ")
+    print (f"total de litros:{valor} ")
 else:
     print ("digite um tipo de combustivel valido")
 
